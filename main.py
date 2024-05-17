249074dbf4745c49639364a585ea43c6a92dbedf
REYMON-LEIN <167728711+REYMON-LEIN@users.noreply.github.com>
2024-05-17 23:39:15 +0800

    Part6.py
    Tugas 6

---
+++ main.py
@@ -0,0 +1,86 @@
+# perbandingan 
+# lebih dari >
+# kurang dari <
+# lebih kurang dari sama dengan >=
+# kurang dari sama dengan <=
+# sama dengan =
+# tidak sama dengan =
+# is
+# is not
+
+
+a = 3
+b = 6
+
+hasil = a > b
+print (hasil)
+print (a, '>', b, '=', hasil) 
+
+hasil = a < b
+print (hasil)
+print (a, '<', b, '=', hasil)
+
+hasil = a >= b
+print (hasil)
+print (a, '>=', b, '=', hasil)
+
+hasil = a <= b
+print (hasil)
+print (a, '<=', b, '=', hasil)
+
+hasil = a == b
+print (hasil)
+print (a, '==', b, '=', hasil)
+
+hasil = a = b
+print (hasil)
+print (a, '=', b, '=', hasil)
+
+hasil = a is b
+print (hasil)
+print (a, 'is', b, '=', hasil)
+
+hasil = a is not b
+print (hasil)
+print (a, 'is not', b, '=', hasil)
+
+
+# lebih dari >
+hasil = a > 7
+print (hasil)
+print (a, '>', 7, '=', hasil) 
+
+# kurang dari <
+hasil = a < 8
+print (hasil)
+print (a, '<', 8, '=', hasil) 
+
+# lebih kurang dari sama dengan >= 
+hasil = a >= 5
+print (hasil)
+print (a, '>=', 5, '=', hasil)
+
+# kurang dari sama dengan <=
+hasil = a <= 4
+print (hasil)
+print (a, '<=', 4, '=', hasil)
+
+# sama dengan =
+hasil = a == b
+print (hasil)
+print (a, '==', b, '=', hasil)
+
+# tidak sama dengan =
+hasil = a = 3
+print (hasil)
+print (a, '=', 3, '=', hasil) 
+
+# is
+hasil = a is 6
+print (hasil)
+print (a, 'is', 6, '=', hasil)
+
+# is not
+hasil = a is not 3
+print (hasil)
+print (a, 'is not', 3, '=', hasil)
