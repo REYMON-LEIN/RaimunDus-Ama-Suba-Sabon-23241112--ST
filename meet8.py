eba517d0451bb68c3b60010cc1fd1bcabd7a818c
REYMON-LEIN <167728711+REYMON-LEIN@users.noreply.github.com>
2024-06-12 23:12:32 +0800

    Add files via upload
    meet8.py

---
+++ meet8.py
@@ -0,0 +1,13 @@
+# percabangan jika maka
+
+nama = input("silakan masukan nama")
+# if kondisi : aksi
+if nama =="reymon" : print("anak ganteng")
+print("program selesai")
+
+
+if nama=="reymon" :
+    print("anak ganteng")
+    print("anak baik")
+print("program selesai")
+print(f"thaks {nama}")
