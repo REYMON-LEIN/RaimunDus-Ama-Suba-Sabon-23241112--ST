8271b96dc75c12eea161c1c7a4752bca3a1544b9
REYMON-LEIN <167728711+REYMON-LEIN@users.noreply.github.com>
2024-06-13 14:19:52 +0800

    Add files via upload
    meet9.py

---
+++ Belajar mulit percabangan.py
@@ -0,0 +1,12 @@
+#Belajar mulit percabangan
+
+nama = input("masukan nama :")
+if nama=="edy": # kondisi 1
+    print("dosen ganteng") # aksi ture 1
+elif nama=="dila" : # kondisi 2
+    print("mahasisiwa cantik") # aksi true 2
+elif nama=="putra": # kondisi 3
+    print ("mahasiswa gk ganteng") # aksi ture 3
+else:
+    print("anda bukan siapa-aiapa")
+print("program selesai")
